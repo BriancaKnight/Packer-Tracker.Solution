766120d7d1731fe0285deb6402eb168fff77123b
Brianca Knight <briancaknight@gmail.com>
2024-01-25 11:32:36 -0800

    add test 12 and pass
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>

--- PackerTracker.Tests/ModelTests/CategoryTests.cs
+++ PackerTracker.Tests/ModelTests/CategoryTests.cs
@@ -8,6 +8,12 @@ namespace ToDoList.Tests
   [TestClass]
   public class CategoryTests
   {
-
+    
+  [TestMethod]
+    public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+    {
+      Category newCategory = new Category("test category");
+      Assert.AreEqual(typeof(Category), newCategory.GetType());
+    }
   }
 }
